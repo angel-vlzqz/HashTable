f419343c20e8274cff6ad903f990659c34d46ff4
angel velazquez <21velazquezang@gmail.com>
2023-04-05 12:29:36 -0700

    simple deleteItem function

--- sources/Hash.cpp
+++ sources/Hash.cpp
@@ -9,12 +9,31 @@ Hash::Hash(int V)
 
 void Hash::insertItem(int data)
 {
+    // get hash index and insert
     int INDEX = hashMe(data);
     table[INDEX].push_back(data);
 }
 
 void Hash::deleteItem(int key)
 {
+    // find hash index
+    int INDEX = hashMe(key);
+
+    //find key in index
+    list<int>::iterator i;
+    for (i = table[INDEX].begin(); i != table[INDEX].end(); i++)
+    {
+        if (*i == key)
+        {
+            break;
+        }
+
+        //if key is found, remove it
+        if (i != table[INDEX].end())
+        {
+            table[INDEX].erase(i);
+        }
+    }
 }
 
 int Hash::hashMe(int key)
