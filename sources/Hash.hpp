0652bc2d11df1f428a7686bb7cd940f5deaa1257
angel velazquez <21velazquezang@gmail.com>
2023-04-05 12:39:26 -0700

    seg fault driver method

--- sources/Hash.hpp
+++ sources/Hash.hpp
@@ -6,12 +6,14 @@ using namespace std;
 class Hash
 {
     int BUCKET = 5971; // number of buckets
-    list<int> *table; // pointer to array with buckets
+    list<int> *table;  // pointer to array with buckets
 
-    public:
-        Hash(int V);
-        void insertItem(int item);
-        void deleteItem(int key);
-        int hashMe(int key);
-        void display();
+public:
+    Hash(int V);
+    void insertItem(int item);
+    void deleteItem(int key);
+    void display();
+
+private:
+    int hashMe(int key);
 };
--- sources/Main.cpp
+++ sources/Main.cpp
@@ -5,4 +5,19 @@ using namespace std;
 
 int main()
 {
+    //array that contains keys
+    int test[] = {15, 11, 27, 8, 12};
+    int num = sizeof(test) / sizeof(test[0]);
+
+    //insert keys
+    Hash hash(5971);
+
+    for (int i = 0; i < num; i++)
+    {
+        hash.insertItem(test[i]);
+    }
+
+    hash.display();
+    hash.deleteItem(12);
+    hash.display();
 }
